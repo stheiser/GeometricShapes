8e5581319a09c8f216cd14782738ac1367b8ae7f
Hizo! <stheiser@tempeunion.org>
2024-01-29 11:09:38 -0700

    Add files via upload

---
+++ GeometricShapes.java
@@ -0,0 +1,36 @@
+
+public class GeometricShapes 
+{
+	private double length;	//
+	private double height;  //
+	
+	//default constructor
+	public GeometricShapes()
+	{
+		length = 0;
+		height = 0;
+	}
+	//parameterized constructor
+	public GeometricShapes(double someLength, double someHeight)
+	{
+		
+	}
+	
+	/**
+	 * This method will calculate the area of a rectangle
+	 * @return  area of rectangle
+	 */
+	public double getArea()
+	{
+		//do something
+	}
+	
+	/**
+	 * This method will calculate the perimeter of a rectangle
+	 * @return perimeter of rectangle
+	 */
+	public double getPerimeter()
+	{
+		//do something
+	}
+}
---
+++ GeometricShapesTest.java
@@ -0,0 +1,20 @@
+//imports go here
+/**
+ * This program is intended for students to complete
+ * Students will intake a length and width for a rectangle
+ * and be able to compute the area and perimeter
+ * @author stheiser
+ *
+ */
+public class GeometricShapesTest {
+	//something important is missing - add it
+	
+	//welcome user & obtain 2 values for length and height of a rectangle
+	
+	//use the values to pass to the other class by object construction
+	GeometricShapes connect = new GeometricShapes(length, width);
+	
+	//return the area and perimeter of the rectangle.
+	
+	//close the scanner or make sure JOption window isn't open anymore
+}
